13e575ef7fa6d54d02ebc82bcd24fe6a16275d2c
keyur154 <72492421+keyur154@users.noreply.github.com>
2021-01-29 12:31:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+var camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(
+        function(data_uri){
+            document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+        }
+
+    );
+}
+
+console.log("ML5 Version:",ml5.version);
+
+classifier.ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/V8rdtFBRR/model.json",modelLoaded);
+
+function modelLoaded() {
+    console.log("modelLoaded");
+}
